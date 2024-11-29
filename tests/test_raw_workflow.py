a754d0f7ff219381cdc570499c5fd18952010095
Fabi <schmidtfa91@gmail.com>
2024-11-29 12:53:41 +0100

    random

--- tests/test_raw_workflow.py
+++ tests/test_raw_workflow.py
@@ -10,6 +10,7 @@ meg_path = os.path.join(sample_data_folder, "MEG", "sample",)
 sample_data_raw_file = os.path.join(meg_path, "sample_audvis_raw.fif")
 raw = mne.io.read_raw_fif(sample_data_raw_file, preload=True)
 
+#%%
 
 
 #%% Lets initialize the almkanal
