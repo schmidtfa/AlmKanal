c514e5aed8282c0e0bf56839cbb8a15d53c46fc4
schmidtfa <schmidtfa91@gmail.com>
2024-12-03 12:43:39 +0000

    dropped stuff

--- template_pipelines/markov_pipeline.py
+++ template_pipelines/markov_pipeline.py
@@ -1,39 +1,39 @@
-#Basic Resting state analysis pipeline
-import mne
-from almkanal import AlmKanal
-from plus_slurm import Job
-import joblib
+# Basic Resting state analysis pipeline
 from pathlib import Path
 
-class RestingPipe(Job):
+import joblib
+import mne
+from plus_slurm import Job
 
+from almkanal import AlmKanal
+
+
+class RestingPipe(Job):
     job_data_folder = 'data_meg'
 
-    def run(self,
-            subject_id: str,
-            data_path: str,
-            subjects_dir: str,
-            empty_room_path: str,
-            lp: float = 100,
-            hp: float = 0.1,
-            ) -> None:
-        
+    def run(
+        self,
+        subject_id: str,
+        data_path: str,
+        subjects_dir: str,
+        empty_room_path: str,
+        lp: float = 100,
+        hp: float = 0.1,
+    ) -> None:
         full_path = Path(data_path) / subject_id + '_resting.fif'
         raw = mne.io.read_raw(full_path, preload=True)
 
         ak = AlmKanal(raw=raw)
         # do raw preproc
         ak.do_maxwell()
-        #% filter
+        # % filter
         ak.raw.filter(l_freq=hp, h_freq=lp)
-        #% run_ica
+        # % run_ica
         ak.do_ica()
         # % do fwd model
-        ak.do_fwd_model(subject_id=subject_id, 
-                        subjects_dir=subjects_dir, 
-                        redo_hdm=True)
+        ak.do_fwd_model(subject_id=subject_id, subjects_dir=subjects_dir, redo_hdm=True)
         # % do epochs
-        #TODO: This is a placeholder built the proper one from here
+        # TODO: This is a placeholder built the proper one from here
         ak.do_events()
         event_dict = {
             'Auditory/Left': 1,
@@ -43,7 +43,6 @@ class RestingPipe(Job):
         }
 
         ak.do_epochs(event_id=event_dict)
-
 
         # % go 2 source
         stc = ak.do_src(
