a181c765637d7043e3ce74ae44c1ecf81fb0d32f
Fabi <schmidtfa91@gmail.com>
2025-07-16 10:59:19 +0200

    fixed ransac bug and added some tests

--- almkanal/__init__.py
+++ almkanal/__init__.py
@@ -3,7 +3,7 @@
 from almkanal.__version__ import __version__
 from almkanal.almkanal import AlmKanal, AlmKanalStep
 from almkanal.preproc_utils.bio_utils import PhysioCleaner
-from almkanal.preproc_utils.channel_utils import RANSAC, Maxwell, MultiBlockMaxwell
+from almkanal.preproc_utils.channel_utils import RANSAC, Maxwell, MultiBlockMaxwell, ReReference
 from almkanal.preproc_utils.epoch_utils import Epochs
 from almkanal.preproc_utils.event_utils import Events
 from almkanal.preproc_utils.filter_utils import Filter, Resample
@@ -19,6 +19,7 @@ __all__ = [
     'Resample',
     'Maxwell',
     'MultiBlockMaxwell',
+    'ReReference',
     'RANSAC',
     'ICA',
     'PhysioCleaner',
